425fe82ecc2a0a97def376232f0493216540b5a0
Cajova-Houba <tkmushroomer@gmail.com>
2018-05-24 22:31:43 +0200

    refs #6953: Castecna oprava migracnich skriptu.

---
+++ db/v06__zaznam_prum_den.sql
@@ -0,0 +1,24 @@
+-- tento skript prida tabulku s prumerem dnu
+--
+-- Struktura tabulky `zaznam_prum_den`
+--
+
+CREATE TABLE IF NOT EXISTS `zaznam_prum_den` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `vozidla_pocet` int(11) NOT NULL,
+  `rychlost_prumer` double NOT NULL,
+  `datum` date NOT NULL,
+  `smer` int(11) NOT NULL,
+  `zarizeni_id` varchar(20) NOT NULL,
+  `vozidla_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `zarizeni_id` (`zarizeni_id`),
+  KEY `vozidla_id` (`vozidla_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Omezení pro tabulku `zaznam_prum_den`
+--
+ALTER TABLE `zaznam_prum_den`
+  ADD CONSTRAINT `zaznam_prum_den_ibfk_2` FOREIGN KEY (`vozidla_id`) REFERENCES `vozidla` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `zaznam_prum_den_ibfk_1` FOREIGN KEY (`zarizeni_id`) REFERENCES `zarizeni` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
--- db/v4__geosouradnice.sql
+++
@@ -1,3 +0,0 @@
--- skript prida sloupecky pro geosouradnice do tabulky ulice
-alter table `prujezd_vozidel`.`ulice` add column `lng` double not null;
-alter table `prujezd_vozidel`.`ulice` add column `lat` double not null;
